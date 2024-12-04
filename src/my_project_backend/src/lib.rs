1e6e4a00e6752317aa5aef7fab6de79e8a2daca0
Norbert Witek <Norbas788@gmail.com>
2024-12-04 09:40:57 +0000

    pawel

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,17 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk::query]
+fn set_msg(new_msg: String){
+    MSG.with(|msg|{
+    msg.borrow_mut() = new_msg
+    })
+}
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
+    
 }
