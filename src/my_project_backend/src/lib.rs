ee4bc7b9e45bc338b152f22cf88749687dfd1e05
Norbert Witek <Norbas788@gmail.com>
2024-12-04 11:10:12 +0000

    pawel

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -4,14 +4,25 @@ thread_local! {
     static MSG: RefCell<String> = RefCell::new(String::new());
 }
 
-#[ic_cdk::query]
-fn set_msg(new_msg: String){
-    MSG.with(|msg|{
-    msg.borrow_mut() = new_msg
+#[ic_cdk::update]
+fn set_msg(new_msg: String) {
+    MSG.with(|msg| {
+        *msg.borrow_mut() = new_msg
     })
 }
+
+#[ic_cdk::query]
+fn get_msg() -> String {
+    MSG.with(|msg| msg.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
-    
 }
+
+// new_msg: String
+// msg.borrow_mut(): &mut String
+// &mut String != String
+// *&mut String == String
+// String == String
